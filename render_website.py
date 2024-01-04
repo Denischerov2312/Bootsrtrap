36c472badd773522ad69b898f9a46019e8d66845
Denischerov2312 <hshshsdbhshs@gmail.com>
2024-01-05 00:13:01 +0300

    Initial commit

---
+++ render_website.py
@@ -0,0 +1,35 @@
+import json
+from livereload import Server
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+def get_books(filepath='books.json'):
+    with open(filepath, 'r', encoding="utf8") as books_file:
+        books_json = books_file.read()
+    books = json.loads(books_json)
+    return books
+
+
+def on_reload(books):
+    books = get_books('25books_json.json')
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    rendered_page = template.render(books=books)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+def main():
+    books = get_books('25books_json.json')
+    on_reload(books)
+
+    server = Server()
+    server.watch('index.html', on_reload)
+    server.serve(root='.')
+
+
+if __name__ == '__main__':
+    main()
