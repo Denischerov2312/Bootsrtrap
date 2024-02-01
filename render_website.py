dbfba73ceb75329043148560317b879e7dcd86af
Denischerov2312 <hshshsdbhshs@gmail.com>
2024-02-01 22:11:49 +0300

    Добавил пагинатор

--- render_website.py
+++ render_website.py
@@ -1,13 +1,35 @@
+import os
+from math import ceil
 import json
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from more_itertools import chunked
 
 
-def get_books(filepath='books.json'):
+def get_books(filepath):
     with open(filepath, 'r', encoding="utf8") as books_file:
         books_json = books_file.read()
     books = json.loads(books_json)
     return books
+
+
+def split_and_save_page_books(books):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+    os.makedirs('pages/', exist_ok=True)
+    pages_of_book = chunked(books, 14)
+    count_page = ceil(len(books) / 14)
+    for number, page in enumerate(pages_of_book):
+        filepath = f'pages/index{number}.html'
+        rendered_page = template.render(books=chunked(page, 2),
+                                        count_page=count_page,
+                                        current_page=1,
+                                        )
+        with open(filepath, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def on_reload(books):
@@ -16,15 +38,19 @@ def on_reload(books):
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-    rendered_page = template.render(books=books)
+    count_page = ceil(len(books) / 14)
+    current_page = 1
+    rendered_page = template.render(books=chunked(books, 2),
+                                    count_page=count_page,
+                                    current_page=current_page,
+                                    )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
 def main():
-    books = get_books('25books_json.json')
-    on_reload(books)
-
+    books = get_books('100books_json.json')
+    split_and_save_page_books(books)
     server = Server()
     server.watch('template.html', lambda: on_reload(books))
     server.serve(root='.')
