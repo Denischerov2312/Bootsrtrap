ce16e1306865be46d2e4ac02fb345926e8d53021
Denischerov2312 <hshshsdbhshs@gmail.com>
2024-04-30 19:47:02 +0300

    Убрал числа без обьяснений в коде

--- render_website.py
+++ render_website.py
@@ -7,6 +7,10 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 
 
+BOOKS_ON_PAGE_COUNT = 14
+BOOK_COUTN_IN_LINE = 2
+
+
 def get_books(filepath):
     with open(filepath, 'r', encoding="utf8") as books_file:
         books = json.load(books_file)
@@ -20,13 +24,13 @@ def split_and_save_page_books(books):
     )
     template = env.get_template('template.html')
     os.makedirs('pages/', exist_ok=True)
-    pages_of_book = chunked(books, 14)
-    page_count = ceil(len(books) / 14)
+    pages_of_book = chunked(books, BOOKS_ON_PAGE_COUNT)
+    page_count = ceil(len(books) / BOOKS_ON_PAGE_COUNT)
     for number, page in enumerate(pages_of_book, 1):
         filepath = f'pages/index{number}.html'
         if number == 1:
             filepath = 'pages/index.html'
-        rendered_page = template.render(books=chunked(page, 2),
+        rendered_page = template.render(books=chunked(page, BOOK_COUTN_IN_LINE),
                                         page_count=page_count,
                                         current_page=number,
                                         )
@@ -40,9 +44,9 @@ def on_reload(books):
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-    page_count = ceil(len(books) / 14)
+    page_count = ceil(len(books) / BOOKS_ON_PAGE_COUNT)
     current_page = 1
-    rendered_page = template.render(books=chunked(books, 2),
+    rendered_page = template.render(books=chunked(books, BOOK_COUTN_IN_LINE),
                                     page_count=page_count,
                                     current_page=current_page,
                                     )
