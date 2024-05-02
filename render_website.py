285072d55dbe8cd17c01d56b007cef0eabbde4fe
Denischerov2312 <hshshsdbhshs@gmail.com>
2024-05-02 20:02:47 +0300

    Починил Livereload

--- render_website.py
+++ render_website.py
@@ -19,7 +19,7 @@ def get_books(filepath):
     return books
 
 
-def split_and_save_page_books(books, dest_folder):
+def on_reload(books, dest_folder):
     dest_folder = f'{dest_folder}/' if dest_folder[-1] != '/' else dest_folder
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -42,23 +42,6 @@ def split_and_save_page_books(books, dest_folder):
             file.write(rendered_page)
 
 
-def on_reload(books, dest_folder):
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-    template = env.get_template('template.html')
-    page_count = ceil(len(books) / BOOKS_ON_PAGE_COUNT)
-    current_page = 1
-    rendered_page = template.render(books=chunked(books, BOOK_COUTN_IN_LINE),
-                                    page_count=page_count,
-                                    current_page=current_page,
-                                    dest_folder=dest_folder,
-                                    )
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-
-
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dest_folder', type=str, default='pages/',
@@ -71,7 +54,7 @@ def get_args():
 def main():
     args = get_args()
     books = get_books(args.books_file)
-    split_and_save_page_books(books, args.dest_folder)
+    on_reload(books, args.dest_folder)
     server = Server()
     server.watch('template.html', lambda: on_reload(books, args.dest_folder))
     server.serve(root='.')
